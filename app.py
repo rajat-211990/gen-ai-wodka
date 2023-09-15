2618a75657a48758ec238a3340abff1fc467d2e7
Icluster <icluster@Iclusters-MacBook-Pro.local>
2023-09-15 14:01:53 +0530

    buildpack

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+import os
 app = Flask(__name__)
 
 @app.route('/')
@@ -31,4 +31,5 @@ def contactus():
     return render_template('contactus.html')
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
